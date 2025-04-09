e33d4c81e8bbf7a67e3c549c1bb945992f2227d8
Shrikant <shrikanttembhe85@gmail.com>
2025-04-09 19:53:31 +0530

    American timezone person has commited

--- src/com/java/exception/practice/ArithematicExceptions.java
+++ src/com/java/exception/practice/ArithematicExceptions.java
@@ -14,10 +14,29 @@ public class ArithematicExceptions {
 		int a = 7 / 0;
 	}
 
+	public static void checkException3() {
+		int a = 6 / 0;
+	}
+
+	public static void checkException4() {
+		int a = 5 / 0;
+	}
+
+	public static void checkException5() {
+		int a = 4 / 0;
+	}
+
 	public static void main(String[] args) {
 
 		checkException();
 
 		checkException2();
+
+		checkException3();
+
+		checkException4();
+
+		checkException5();
+
 	}
 }
